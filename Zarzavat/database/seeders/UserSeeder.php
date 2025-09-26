f991a7dffeeb993948c3da8ae61684557c913fe5
Milen03 <milen.atanasovv03@gmail.com>
2025-09-26 19:38:42 +0300

    Make user seeder

--- Zarzavat/database/seeders/UserSeeder.php
+++ Zarzavat/database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // This is Admin User
+        User::create([
+            'name'=>'Admin',
+            'email'=>'milen.atanasovv03@gmail.com',
+            'password'=> Hash::make('71750300qQ'),
+            'role'=>'admin'
+        ]);
+
+         User::create([
+            'name' => 'Client User',
+            'email' => 'geri07@gmail.com',
+            'password' => Hash::make('123456'),
+            'role' => 'user',
+        ]);
     }
 }
