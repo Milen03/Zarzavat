67103020f692c6ce9cdc45592df6759699381bb3
Milen03 <milen.atanasovv03@gmail.com>
2025-10-02 20:18:44 +0300

    Add Admin Controler

---
+++ Zarzavat/app/Http/Controllers/Admin/AdminContoller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
+
+
+class AdminContoller extends Controller
+{
+    public function index(){
+        return view('admin.dashboard',[
+             'productsCount' => Product::count(),
+            'ordersCount' => Order::count(),
+            'usersCount' => User::count(),
+        ]);
+    }
+}
