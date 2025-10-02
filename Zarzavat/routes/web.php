2541487c48d2fb1c81edb0217f0ad5d43fc8d609
Milen03 <milen.atanasovv03@gmail.com>
2025-10-02 20:18:58 +0300

    Add router for Admin

--- Zarzavat/routes/web.php
+++ Zarzavat/routes/web.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
 
+use App\Http\Controllers\Admin\AdminContoller;
+use App\Http\Controllers\ProductController as AdminProductController;
+use App\Http\Controllers\OrderController as AdminOrderController;
+
+
+//Auth
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
@@ -14,15 +20,29 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+//Admin
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [AdminContoller::class, 'index'])->name('dashboard');
 
+    // Продукти
+    Route::resource('products', AdminProductController::class);
+
+    // Поръчки
+    Route::resource('orders', AdminOrderController::class)->only(['index', 'show', 'update']);
+});
+
+
+//products
 Route::get('/', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
 
+
+//Cart
 Route::get('/cart' , [CartContoller::class,'index'])->name('cart.index');
 Route::post('/cart/add/{id}', [CartContoller::class, 'add'])->name('cart.add');
 Route::post('/cart/update/{id}', [CartContoller::class, 'update'])->name('cart.update');
 Route::post('/cart/remove/{id}', [CartContoller::class, 'remove'])->name('cart.remove');
 
-
+//checkout
 Route::get('/checkout',[OrderController::class , 'checkout'])->name('checkout');
 Route::post('/checkout' , [OrderController::class , 'placeOrder'])->name('checkout.place');
