f7ebfc4815c488ad3d36c87adc820f1919f54ec3
Milen03 <milen.atanasovv03@gmail.com>
2025-10-22 19:49:23 +0300

    Product validacion

--- Zarzavat/app/Http/Controllers/Admin/ProductController.php
+++ Zarzavat/app/Http/Controllers/Admin/ProductController.php
@@ -3,79 +3,64 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
 use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Http\Request;
 
-class ProductController extends Controller{
-     // Списък продукти
-    public function index(){
+class ProductController extends Controller
+{
+    // Списък продукти
+    public function index()
+    {
         $products = Product::all();
         return view('admin.products.index', compact('products'));
     }
 
-    //Форма за създаване 
-    public function create(){
-    $categories = \App\Models\Category::all();
-    return view('admin.products.create', compact('categories'));
-}
-
-    //Запис на нов продукт
-    public function store(Request $request){
-
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'price' => 'required|numeric',
-            'stock' => 'required|numeric',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-             'category_id' => 'required|exists:categories,id',
-        ]);
-
-        $data = $request->except('image');
-    
-    // Handle image upload
-    if ($request->hasFile('image')) {
-        $imagePath = $request->file('image')->store('products', 'public');
-        $data['image'] = $imagePath;
+    // Форма за създаване 
+    public function create()
+    {
+        $categories = Category::all();
+        return view('admin.products.create', compact('categories'));
     }
+
+    // Запис на нов продукт
+    public function store(ProductRequest $request)
+    {
+        $data = $request->validated();
+
+        // Обработка на изображението
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('products', 'public');
+            $data['image'] = $imagePath;
+        }
 
         Product::create($data);
 
         return redirect()->route('admin.products.index')->with('success', 'Продуктът е добавен.');
     }
 
-    //edit
-    public function edit(Product $product){
+    // Форма за редакция
+    public function edit(Product $product)
+    {
         $categories = Category::all();
         return view('admin.products.edit', compact('product', 'categories'));
     }
 
-     public function update(Request $request, Product $product)
+    // Обновяване на продукта
+    public function update(ProductRequest $request, Product $product)
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'price' => 'required|numeric',
-            'stock' => 'required|numeric',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'category_id' => 'required|exists:categories,id',
-        ]);
+        $data = $request->validated();
 
-        $data = $request->except('image');
-    
-    // Handle image upload
-    if ($request->hasFile('image')) {
-        $imagePath = $request->file('image')->store('products', 'public');
-        $data['image'] = $imagePath;
-    }
-
+        // Обработка на изображението
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('products', 'public');
+            $data['image'] = $imagePath;
+        }
 
         $product->update($data);
 
         return redirect()->route('admin.products.index')->with('success', 'Продуктът е обновен.');
     }
-    
 
     // Изтриване
     public function destroy(Product $product)
---
+++ Zarzavat/app/Http/Requests/ProductRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class ProductRequest extends FormRequest
+
+
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return  Auth::check() && Auth::user()->role === 'admin';;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+          return [
+           'name' => 'required|string|min:2|max:255',
+            'description' => 'nullable|string|max:500',
+            'price' => 'required|numeric|min:0',
+            'image' => 'nullable|image|mimes:jpg,png,jpeg|max:2048',
+            'stock' => 'required|integer|min:0',
+        ];
+    }
+
+     public function messages(): array
+    {
+        return [
+            'name.required' => 'Името на продукта е задължително.',
+            'price.required' => 'Цената е задължителна.',
+            'image.image' => 'Каченият файл трябва да е изображение.',
+        ];
+    }
+}
