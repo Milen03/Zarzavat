e7a58337e59905e87f7a07e4fdb8dd0112c97ace
Milen03 <milen.atanasovv03@gmail.com>
2025-10-22 19:13:09 +0300

    Make validacion

--- Zarzavat/app/Http/Controllers/Auth/RegisterController.php
+++ Zarzavat/app/Http/Controllers/Auth/RegisterController.php
@@ -20,26 +20,12 @@ class RegisterController extends Controller
 
     public function register(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
-            'password_confirmation' => 'required',
-        ], [
-            'name.required' => 'Името е задължително',
-            'email.required' => 'Имейлът е задължителен',
-            'email.email' => 'Невалиден имейл адрес',
-            'email.unique' => 'Този имейл вече е регистриран',
-            'password.required' => 'Паролата е задължителна',
-            'password.min' => 'Паролата трябва да е поне 8 символа',
-            'password.confirmed' => 'Паролите не съвпадат',
-            'password_confirmation.required' => 'Моля, потвърдете паролата',
-        ]);
-
+        $data = $request->validate();
+        
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => bcrypt($data['password']),
             'role' => 'user', // По подразбиране обикновен потребител
         ]);
         
