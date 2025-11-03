f250e294e329399b39d33c4344eff788eb42649a
Milen03 <milen.atanasovv03@gmail.com>
2025-11-03 20:55:16 +0200

    description returne null

--- Zarzavat/database/migrations/2025_09_23_170819_products.php
+++ Zarzavat/database/migrations/2025_09_23_170819_products.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->decimal('price', 8, 2);
             $table->float('stock');
             $table->string('image')->nullable();
